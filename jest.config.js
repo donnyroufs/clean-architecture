dd327574066d0d007553eaccb71120b53707c4d1
Donny Roufs <iamchets@yahoo.com>
2020-12-09 16:50:49 +0100

    add alias support for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
+const tsconfig = require("./tsconfig.json");
+const moduleNameMapper = require("tsconfig-paths-jest")(tsconfig);
+
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  modulePathIgnorePatterns: ["src/entrypoint/__tests__/mocks"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePathIgnorePatterns: ["src/presentation/api/__tests__/mocks"],
+  moduleNameMapper,
 };
